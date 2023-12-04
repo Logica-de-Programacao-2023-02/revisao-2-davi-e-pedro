96d6348b89f75eb6b708731b34378d975918b230
davv12 <142831628+davv12@users.noreply.github.com>
2023-12-04 18:47:52 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -3,6 +3,40 @@ package q4
 // Você recebe uma lista de caminhos, onde `caminhos[i] = [cidadeAi, cidadeBi]` significa que existe um caminho direto que
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
-func Destino(caminhos [][2]string) (string, error) {
-	return "", nil
+
+import "fmt"
+
+func encontrarCidadeDestino(caminhos [][]string) (string, error) {
+	cidadesEntrada := make(map[string]struct{})
+	cidadesSaida := make(map[string]struct{})
+
+	for _, caminho := range caminhos {
+		cidadesEntrada[caminho[0]] = struct{}{}
+		cidadesSaida[caminho[1]] = struct{}{}
+	}
+
+	for _, caminho := range caminhos {
+		if _, ok := cidadesEntrada[caminho[1]]; !ok {
+			return caminho[1], nil
+		}
+	}
+
+	return "", fmt.Errorf("nenhuma cidade de destino encontrada")
+}
+
+func main() {
+	caminhos := [][]string{
+		{"London", "New York"},
+		{"New York", "Lima"},
+		{"Lima", "Sao Paulo"},
+	}
+
+	cidadeDestino, err := encontrarCidadeDestino(caminhos)
+
+	if err != nil {
+		fmt.Println("Erro:", err)
+		return
+	}
+
+	fmt.Println("Cidade de Destino:", cidadeDestino)
 }
